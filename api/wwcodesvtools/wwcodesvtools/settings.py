e2738a90b1191e6ec1869c3943326f6b0d7c9be1
Sarah Sanger <ssanger2020@gmail.com>
2022-11-03 17:42:49 -0700

    Update settings.py

--- api/wwcodesvtools/wwcodesvtools/settings.py
+++ api/wwcodesvtools/wwcodesvtools/settings.py
@@ -31,7 +31,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = env('DEBUG')
+DEBUG = os.environ.get('DEBUG')
 
 ALLOWED_HOSTS = []
 RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
