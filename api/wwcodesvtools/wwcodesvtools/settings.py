d8c887be2a65c4a6f01f2d1ea015d93a5ba620f5
Sarah Sanger <ssanger2020@gmail.com>
2022-11-03 17:16:42 -0700

    Update settings.py

--- api/wwcodesvtools/wwcodesvtools/settings.py
+++ api/wwcodesvtools/wwcodesvtools/settings.py
@@ -32,7 +32,10 @@ SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env('DEBUG')
 
-ALLOWED_HOSTS = ['wwcode-chtools-api.herokuapp.com', 'localhost']
+ALLOWED_HOSTS = []
+RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
+if RENDER_EXTERNAL_HOSTNAME:
+    ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
 
 # Application definition
 
