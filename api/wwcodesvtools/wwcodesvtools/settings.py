f5d12ec3432eb408c370d66ad691170fd0480d14
Sarah Sanger <ssanger2020@gmail.com>
2022-11-03 18:49:13 -0700

    Update settings.py

--- api/wwcodesvtools/wwcodesvtools/settings.py
+++ api/wwcodesvtools/wwcodesvtools/settings.py
@@ -92,7 +92,6 @@ WSGI_APPLICATION = 'wwcodesvtools.wsgi.application'
 
 DATABASES = {
     # read os.environ['DATABASE_URL'] and raises ImproperlyConfigured exception if not found
-#     'default': env.db(), 
     'default': os.environ.get(db()),
 }
 
